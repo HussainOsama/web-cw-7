b6fe93b1e12faf6dd36c4d055b0029426c9c659c
HussainOsama <hussain.osama.asd@gmail.com>
2021-01-06 21:55:46 +0300

    Done!

--- question1.js
+++ question1.js
@@ -1,1 +1,25 @@
-// قم بحل السؤال الأول هنا
+
+
+
+function hello() {
+
+    var pets = ["Cats", "Lions", "Dogs", "dolphin"];
+    pets.pop()
+    pets.push("Ants")
+    console.log(pets);
+    pets[2] = "elephant"
+    console.log(pets);
+
+    var cat = { 
+        name: "flufy", 
+        age: 12, 
+        kind: "tiger"
+    }
+    console.log(cat);
+    cat.emoji = "😻"
+    console.log(cat);
+    console.log(cat.age);
+}
+
+hello()
+
